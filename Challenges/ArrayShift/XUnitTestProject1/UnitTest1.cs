4691896b88ed5c95f0b3e5d085f64aae18cb9028
andrewquamme <andrewquamme@gmail.com>
2019-01-09 09:59:01 -0800

    Test2 pass

--- Challenges/ArrayShift/XUnitTestProject1/UnitTest1.cs
+++ Challenges/ArrayShift/XUnitTestProject1/UnitTest1.cs
@@ -13,5 +13,14 @@ namespace XUnitTestProject1
             int[] returnArray = { 1, 2, 5, 3, 4 };
             Assert.Equal(returnArray, Program.ArrayShift(array, 5));
         }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] array = { 2,4,6,8 };
+            int[] returnArray = {2,4,5,6,8 };
+            Assert.Equal(returnArray, Program.ArrayShift(array, 5));
+        }
+
     }
 }
