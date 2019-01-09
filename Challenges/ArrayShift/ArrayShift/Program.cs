10501c35d23d5d338b23587527a0c61aba2b02aa
andrewquamme <andrewquamme@gmail.com>
2019-01-09 09:39:07 -0800

    codes ArrayShift method

--- Challenges/ArrayShift/ArrayShift/Program.cs
+++ Challenges/ArrayShift/ArrayShift/Program.cs
@@ -6,7 +6,28 @@ namespace ArrayShift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        static int[] ArrayShift(int[] array, int num)
+        {
+            int arrayLength = array.Length;
+            int arrayMidpoint = arrayLength / 2;
+            int[] newArray = { arrayLength + 1 };
+
+            for(int i = 0; i < arrayMidpoint; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            newArray[arrayMidpoint] = num;
+
+            for(int i = arrayMidpoint + 1; i < arrayLength; i++)
+            {
+                newArray[i] = array[i - 1];
+            }
+
+            return newArray;
         }
     }
 }
--- Challenges/ArrayShift/README.md
+++ Challenges/ArrayShift/README.md
@@ -3,6 +3,7 @@
 
 ## Challenge
 <!-- Description of the challenge -->
+Write a function called `InsertShiftArray` which takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
