c9288fbcdf5891ac534bbda89f21f9223d368ded
andrewquamme <andrewquamme@gmail.com>
2019-01-09 19:18:26 -0800

    codes BinarySearch and tests

--- Challenges/BinarySearch/XUnitTestProject1/UnitTest1.cs
+++ Challenges/BinarySearch/XUnitTestProject1/UnitTest1.cs
@@ -7,9 +7,17 @@ namespace XUnitTestProject1
     public class BinarySearchTest
     {
         [Fact]
+        public void TestNumberFoundAtMid()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            Assert.Equal(2, Program.BinarySearch(array, 3));
+        }
+
+        [Fact]
         public void TestNumberFound()
         {
-
+            int[] array = { 1, 2, 3, 4, 5 };
+            Assert.Equal(4, Program.BinarySearch(array, 5));
         }
     }
 }
