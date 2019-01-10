a6d676011a795f4c5ad749af4840fabb37738413
andrewquamme <andrewquamme@gmail.com>
2019-01-10 09:54:41 -0800

    adds tests from examples in challenge document

--- Challenges/BinarySearch/BinarySearchTDD/UnitTest1.cs
+++ Challenges/BinarySearch/BinarySearchTDD/UnitTest1.cs
@@ -4,8 +4,16 @@ using BinarySearch;
 
 namespace BinarySearchTDD
 {
-    public class BinarySearchTest
+    public class TestFoundInArray
     {
+
+        [Fact]
+        public void TestFoundExampleInput()
+        {
+            int[] array = {4, 8, 15, 16, 23, 42};
+            Assert.Equal(2, Program.BinarySearch(array, 15));
+        }
+
         [Fact]
         public void TestNumberFoundAtMid()
         {
@@ -26,12 +34,22 @@ namespace BinarySearchTDD
             int[] array = { 1, 2, 3, 4, 5 };
             Assert.Equal(0, Program.BinarySearch(array, 1));
         }
+    }
 
+    public class TestNotFoundInArray
+    {
         [Fact]
         public void TestNumberNotFound()
         {
             int[] array = { 1, 2, 3, 4, 5 };
             Assert.Equal(-1, Program.BinarySearch(array, 6));
+        }
+
+        [Fact]
+        public void TestNotFoundExampleInput()
+        {
+            int[] array = { 11, 22, 33, 44, 55, 66, 77 };
+            Assert.Equal(-1, Program.BinarySearch(array, 90));
         }
     }
 }
