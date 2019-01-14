d66f81512735b135d8c649d8cdaa04367010fa29
andrewquamme <andrewquamme@gmail.com>
2019-01-14 09:56:56 -0800

    tests instantiation of lists

--- Data-Structures/LinkedList/LinkedListTests/UnitTest1.cs
+++ Data-Structures/LinkedList/LinkedListTests/UnitTest1.cs
@@ -1,14 +1,42 @@
 using System;
 using Xunit;
+using LinkedList.Classes;
 
 namespace LinkedListTests
 {
-    public class UnitTest1
+
+    public class InstantiationTests
     {
         [Fact]
-        public void Test1()
+        public void TestForLList()
         {
+            LList list = new LList();
+            Type expected = typeof(LList);
+            Assert.IsType(expected, list);
+        }
 
+        [Fact]
+        public void TestForNotNull()
+        {
+            LList list = new LList();
+            Assert.NotNull(list.Head);
+        }
+    }
+
+    public class InsertTests
+    {
+        [Fact]
+        public void TestWithoutValue()
+        {
+            LList list = new LList();
+            Assert.True(list.Includes(0));
+        }
+
+        [Fact]
+        public void TestWithValue()
+        {
+            LList list = new LList(123);
+            Assert.True(list.Includes(123));
         }
     }
 }
