8047baa16b13feb0ee1fe2eff7ef4e8b996060e3
andrewquamme <andrewquamme@gmail.com>
2019-01-16 10:21:18 -0800

    tests for exceptions

--- Challenges/LinkedListKthFromEnd/LinkedListKthFromEndTDD/UnitTest1.cs
+++ Challenges/LinkedListKthFromEnd/LinkedListKthFromEndTDD/UnitTest1.cs
@@ -29,4 +29,25 @@ namespace LinkedListKthFromEndTDD
             Assert.Equal(2, Program.KthFromEnd(list, 0));
         }
     }
+
+    public class TestExceptions
+    {
+        [Fact]
+        public void TestNullException()
+        {
+            LList list = new LList();
+            Assert.Equal(-1, Program.KthFromEnd(list, 3));
+        }
+
+        [Fact]
+        public void TestKTooLargeException()
+        {
+            LList list = new LList();
+            list.Append(1);
+            list.Append(3);
+            list.Append(8);
+            list.Append(2);
+            Assert.Equal(-1, Program.KthFromEnd(list, 6));
+        }
+    }
 }
