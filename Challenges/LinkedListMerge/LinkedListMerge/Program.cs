f8dc04caba661dba39ec8f51818f0d3741042c52
andrewquamme <andrewquamme@gmail.com>
2019-01-17 10:25:03 -0800

    codes MergeLists and adds summary comment

--- Challenges/LinkedListMerge/LinkedListMerge/Program.cs
+++ Challenges/LinkedListMerge/LinkedListMerge/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 
+using LinkedList.Classes;
+
 namespace LinkedListMerge
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LList list1 = new LList();
+            list1.Append(1);
+            list1.Append(3);
+            list1.Append(2);
+            list1.Print();
+            Console.WriteLine();
+            LList list2 = new LList();
+            list2.Append(5);
+            list2.Append(9);
+            list2.Append(2);
+            list2.Print();
+            Console.WriteLine();
+            LList mergedList = MergeLists(list1, list2);
+            mergedList.Print();
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Take two linked lists as input and returns a
+        /// merged linked list
+        /// </summary>
+        /// <param name="list1">Linked List 1</param>
+        /// <param name="list2">Linked List 2</param>
+        /// <returns>Merged List</returns>
+        static LList MergeLists(LList list1, LList list2)
+        {
+            list1.Current = list1.Head;
+            list2.Current = list2.Head;
+
+            while (list1.Current != null || list2.Current != null)
+            {
+                Node tempNode = list1.Current.Next;
+                list1.Current.Next = list2.Current;
+                list2.Current = tempNode;
+                list1.Current = list1.Current.Next;
+            }
+            return list1;
         }
     }
 }
---
+++ Challenges/LinkedListMerge/XUnitTestProject1/LLMergeTDD.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using LinkedList.Classes;
+using LinkedListMerge;
+
+namespace LLMergeTDD
+{
+    public class LLMergeTDD
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
