3cf86ff1fb34a18ff4b0e3262146a47f6b8f10c2
andrewquamme <andrewquamme@gmail.com>
2019-01-19 13:15:48 -0800

    write test for stack push

--- Data-Structures/StacksAndQueues/StacksAndQueuesTDD/UnitTest1.cs
+++ Data-Structures/StacksAndQueues/StacksAndQueuesTDD/UnitTest1.cs
@@ -12,5 +12,24 @@ namespace StacksAndQueuesTDD
             Stack stack = new Stack();
             Assert.Null(stack.Top);
         }
+
+        [Fact]
+        public void TestPushToEmptyStack()
+        {
+            Stack stack = new Stack();
+            stack.Push(5);
+            Assert.Equal(5, stack.Top.Value);
+        }
+
+        [Fact]
+        public void TestPushToStackWithNode()
+        {
+            Node node = new Node(1);
+            Stack stack = new Stack(node);
+            stack.Push(2);
+            int[] expected = { 2, 1 };
+            int[] actual = { stack.Top.Value, stack.Top.Next.Value };
+            Assert.Equal(expected, actual);
+        }
     }
 }
