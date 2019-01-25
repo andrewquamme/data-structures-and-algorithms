241abe7af41f562de962d970f04b483a053c9e67
andrewquamme <andrewquamme@gmail.com>
2019-01-24 18:32:42 -0800

    set up files and write code from whiteboard

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Classes/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiBracketValidation.Classes
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+
+        /// <summary>
+        /// Set top to null if no value is passed
+        /// upon instantiation
+        /// </summary>
+        public Stack()
+        {
+            Top = null;
+        }
+
+        /// <summary>
+        /// Create a new Node and push to 
+        /// top of the stack
+        /// </summary>
+        /// <param name="value">value of new Node</param>
+        public void Push(char value)
+        {
+            Node node = new Node(value);
+            node.Next = Top;
+            Top = node;
+        }
+
+        /// <summary>
+        /// Pop top node from the stack and 
+        /// return value to the user
+        /// </summary>
+        /// <returns>Top Node Value</returns>
+        public Node Pop()
+        {
+            // create a temp
+            Node temp = Top;
+            //set top to the next node in the stack
+            Top = Top.Next;
+            // clear the reference of next on the temp
+            temp.Next = null;
+            // return the node
+            return temp;
+        }
+
+        /// <summary>
+        /// Peek at the top Node on
+        /// the stack
+        /// </summary>
+        /// <returns>Top Node Value</returns>
+        public Node Peek()
+        {
+            return Top;
+        }
+    }
+}
---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,39 @@
+﻿using MultiBracketValidation.Classes;
+using System;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool MultiBrackValidation(string input)
+        {
+            Stack stack = new Stack();
+
+            foreach (char c in input)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Peek().Value != c)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
