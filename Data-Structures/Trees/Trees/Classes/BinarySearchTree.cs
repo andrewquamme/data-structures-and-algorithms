205d4c0f7b04ea5a4398e95199c75c89978dbdab
andrewquamme <andrewquamme@gmail.com>
2019-01-28 13:25:41 -0800

    Add BST Contains()

--- Data-Structures/Trees/Trees/Classes/BinarySearchTree.cs
+++ Data-Structures/Trees/Trees/Classes/BinarySearchTree.cs
@@ -6,6 +6,26 @@ namespace Trees.Classes
 {
     public class BinarySearchTree : BinaryTree
     {
-
+         public bool Contains(int value)
+         {
+            Node current = Root;
+            while (current != null)
+            {
+                if (current.Value == value)
+                {
+                    return true;
+                }
+                else if (value < current.Value)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+            return false;
+         }
+        
     }
 }
--- Data-Structures/Trees/Trees/Classes/Tree.cs
+++ Data-Structures/Trees/Trees/Classes/Tree.cs
@@ -7,5 +7,17 @@ namespace Trees.Classes
     public class Tree
     {
         public Node Root { get; set; }
+
+        public Tree()
+        {
+            Root = null;
+        }
+
+        public Tree(Node root)
+        {
+            Root = root;
+        }
+
+
     }
 }
