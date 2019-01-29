5c5630768e4df15a0f49f1b86d4b85599229e06c
andrewquamme <andrewquamme@gmail.com>
2019-01-29 11:58:49 -0800

    finish writing method, adds whiteboard to README

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Classes/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTree.Classes
+{
+    public class Node
+    {
+        public Object Value { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+
+        /// <summary>
+        /// Set value of Node and Left/Right to null
+        /// </summary>
+        /// <param name="value">Value of Node</param>
+        public Node(Object value)
+        {
+            Value = value;
+            Left = Right = null;
+        }
+    }
+}
---
+++ Challenges/FizzBuzzTree/README.md
@@ -0,0 +1,12 @@
+# FizzBuzz Tree
+<!-- Short summary or background information -->
+
+## Challenge
+<!-- Description of the challenge -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Embedded whiteboard image -->
+![Fizz Buzz Tree](../../assets/FizzBuzzTree.jpeg)
