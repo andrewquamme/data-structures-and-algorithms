5750743a8e2c550e63aae67ead3348b5459dc0e4
andrewquamme <andrewquamme@gmail.com>
2019-01-30 07:58:18 -0800

    write tests for FizzBuzzTree

---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Classes/Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirstTraversal.Classes
+{
+    public class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+
+        public Queue()
+        {
+            Front = null;
+        }
+
+        /// <summary>
+        /// Create a new Node and add
+        /// to the back of the queue
+        /// </summary>
+        /// <param name="value">value of new Node</param>
+        public void Enqueue(Node root)
+        {
+            Front = root;
+            //if (Front == null)
+            //{
+            //    Front = node;
+            //    Rear = node;
+            //}
+            //else
+            //{
+            //    Rear.Next = node;
+            //    Rear = node;
+            //}
+        }
+
+        /// <summary>
+        /// Dequeue front node and return
+        /// value to the user
+        /// </summary>
+        /// <returns>Front Node Value</returns>
+        public Node Dequeue()
+        {
+            // create a temp to keep track of the original front
+            Node temp = Front;
+            // change our front reference to the next node
+            Front = Front.Next;
+            // clear our next reference for standalone
+            temp.Next = null;
+
+            //return the temp to the user
+            return temp;
+        }
+
+        /// <summary>
+        /// Peek at the front node in the queue
+        /// </summary>
+        /// <returns>Front Node Value</returns>
+        public Node Peek()
+        {
+            return Front;
+        }
+    }
+}
--- Challenges/FizzBuzzTree/FizzBuzzTreeTDD/UnitTest1.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTreeTDD/UnitTest1.cs
@@ -8,7 +8,15 @@ namespace FizzBuzzTreeTDD
     public class FizzBuzzTreeTests
     {
         [Fact]
-        public void Test1()
+        public void TestNull()
+        {
+            Tree testTree = new Tree();
+            Program.FizzBuzzTree(testTree.Root);
+            Assert.Null(testTree.Root);
+        }
+
+        [Fact]
+        public void TestOutput()
         {
             Tree testTree = new Tree();
             testTree.Root = new Node(3);
@@ -21,6 +29,34 @@ namespace FizzBuzzTreeTDD
             Object[] expected = {
                 "Fizz",
                 2,
+                "Buzz",
+                "FizzBuzz",
+                "FizzBuzz" };
+
+            Object[] results = {
+                testTree.Root.Value,
+                testTree.Root.Left.Value,
+                testTree.Root.Right.Value,
+                testTree.Root.Left.Left.Value,
+                testTree.Root.Left.Right.Value };
+
+            Assert.Equal(expected, results);
+        }
+
+        [Fact]
+        public void TestTreeWithNegatives()
+        {
+            Tree testTree = new Tree();
+            testTree.Root = new Node(-3);
+            testTree.Root.Left = new Node(-2);
+            testTree.Root.Right = new Node(-5);
+            testTree.Root.Left.Left = new Node(-15);
+            testTree.Root.Left.Right = new Node(-30);
+            Program.FizzBuzzTree(testTree.Root);
+
+            Object[] expected = {
+                "Fizz",
+                -2,
                 "Buzz",
                 "FizzBuzz",
                 "FizzBuzz" };
