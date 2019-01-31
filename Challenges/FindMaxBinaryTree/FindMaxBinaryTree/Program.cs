822e98cfc5f78729778bfb29f469af2f2d2c846a
andrewquamme <andrewquamme@gmail.com>
2019-01-30 18:55:28 -0800

    write some code to verify outputs

--- Challenges/FindMaxBinaryTree/FindMaxBinaryTree/Program.cs
+++ Challenges/FindMaxBinaryTree/FindMaxBinaryTree/Program.cs
@@ -10,23 +10,39 @@ namespace FindMaxBinaryTree
             Tree tree = new Tree();
             tree.Root = new Node(1);
             tree.Root.Left = new Node(2);
-            tree.Root.Right = new Node(3);
+            tree.Root.Left.Left = new Node(3);
+            tree.Root.Right = new Node(4);
+            tree.Root.Right.Right = new Node(5);
+            Console.WriteLine(FindMaxValue(tree));
         }
 
-
-        public static int FindMaxValue(Tree tree)
+        /// <summary>
+        /// Takes in a binary tree and traverses in-order
+        /// to find max value in the tree. Returns null if
+        /// tree is empty
+        /// </summary>
+        /// <param name="tree">Binary Tree</param>
+        /// <returns>Max Value</returns>
+        public static int? FindMaxValue(Tree tree)
         {
             int max = 0;
-            if (tree.Root == null) return max;
+            if (tree.Root == null) return null;
             max = InOrder(tree.Root, max);
             return max;
         }
 
+        /// <summary>
+        /// Perform in-order traversal of a tree from root,
+        /// compare value at Node to current max
+        /// </summary>
+        /// <param name="root">Root Node</param>
+        /// <param name="max">Current Max in tree</param>
+        /// <returns></returns>
         public static int InOrder(Node root, int max)
         {
-            if (root.Left != null) InOrder(root.Left, max);
+            if (root.Left != null) max = InOrder(root.Left, max);
             if (root.Value > max) max = root.Value;
-            if (root.Right != null) InOrder(root.Right, max);
+            if (root.Right != null) max = InOrder(root.Right, max);
             return max;
         }
 
