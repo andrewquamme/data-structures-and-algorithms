766497245c2f8c2278a04ce21a0f2bd5578ea2e1
andrewquamme <andrewquamme@gmail.com>
2019-01-30 18:50:04 -0800

    write FindMaxValue method

--- Challenges/FindMaxBinaryTree/FindMaxBinaryTree/Program.cs
+++ Challenges/FindMaxBinaryTree/FindMaxBinaryTree/Program.cs
@@ -7,13 +7,28 @@ namespace FindMaxBinaryTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tree tree = new Tree();
+            tree.Root = new Node(1);
+            tree.Root.Left = new Node(2);
+            tree.Root.Right = new Node(3);
         }
 
 
-        public static int FindMaxValue(Node root)
+        public static int FindMaxValue(Tree tree)
         {
-
+            int max = 0;
+            if (tree.Root == null) return max;
+            max = InOrder(tree.Root, max);
+            return max;
         }
+
+        public static int InOrder(Node root, int max)
+        {
+            if (root.Left != null) InOrder(root.Left, max);
+            if (root.Value > max) max = root.Value;
+            if (root.Right != null) InOrder(root.Right, max);
+            return max;
+        }
+
     }
 }
--- Challenges/FindMaxBinaryTree/README.md
+++ Challenges/FindMaxBinaryTree/README.md
@@ -4,7 +4,7 @@ Find the Maximum Value in a Binary Tree
 
 ## Challenge
 <!-- Description of the challenge -->
-Write a function called `FindMaximumValue` which takes binary tree as its only input. Without utilizing any of the built-in methods available to your language, return the maximum value stored in the tree. You can assume that the values stored in the Binary Tree will be numeric.
+Write a function called `FindMaxValue` which takes binary tree as its only input. Without utilizing any of the built-in methods available to your language, return the maximum value stored in the tree. You can assume that the values stored in the Binary Tree will be numeric.
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
