3624d3a6fce3b0c05b7763c642b43ff34f955919
andrewquamme <andrewquamme@gmail.com>
2019-02-18 11:51:12 -0800

    write code

--- Challenges/InsertionSort/InsertionSort/Program.cs
+++ Challenges/InsertionSort/InsertionSort/Program.cs
@@ -6,12 +6,25 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] testArr = { 4, 3, 1, 8, 5, 7, 2 };
+            InsertionSort(testArr);
         }
 
-        public void InsertionSort()
+        public static void InsertionSort(int[] arr)
         {
+            for(int i=1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
 
+                while (j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+
+                arr[j + 1] = temp;
+            }
         }
     }
 }
