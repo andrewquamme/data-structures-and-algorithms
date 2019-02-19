412bffcd315465e15606a86e7a5a988202d57831
andrewquamme <andrewquamme@gmail.com>
2019-02-18 18:07:59 -0800

    refactor Add, tests still pass

--- Data-Structures/Hashtable/Hashtable/Classes/Hashtable.cs
+++ Data-Structures/Hashtable/Hashtable/Classes/Hashtable.cs
@@ -25,17 +25,40 @@ namespace HashTable.Classes
             return index;
         }
 
+        /// <summary>
+        /// Adds Key/Value to Hash Table
+        /// </summary>
+        /// <param name="key">Key to add</param>
+        /// <param name="value">Value to add</param>
         public void Add(string key, string value)
         {
-            HT[Hash(key)] = new Node(key, value);
+            if (Contains(key))
+            {
+                HT[Hash(key)].Next = new Node(key, value);
+            }
+            else
+            {
+                HT[Hash(key)] = new Node(key, value);
+            }
         }
 
+        /// <summary>
+        /// Takes a key and return value from Hash Table.
+        /// Returns null if not found.
+        /// </summary>
+        /// <param name="key">Key to find</param>
+        /// <returns>Value</returns>
         public string Get(string key)
         {
             if(Contains(key)) return HT[Hash(key)].Value;
-            return "";
+            return null;
         }
 
+        /// <summary>
+        /// Checks if key is in Hash Table
+        /// </summary>
+        /// <param name="key">Key to find</param>
+        /// <returns>True/False</returns>
         public bool Contains(string key)
         {
             if (HT[Hash(key)] != null) return true;
