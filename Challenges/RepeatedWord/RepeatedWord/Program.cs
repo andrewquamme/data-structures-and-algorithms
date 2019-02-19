8171c5eecdec9770b9eaa80885150525bae48ac9
andrewquamme <andrewquamme@gmail.com>
2019-02-19 09:59:14 -0800

    quick test of code does not work

--- Challenges/RepeatedWord/RepeatedWord/Program.cs
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -2,11 +2,25 @@
 
 namespace RepeatedWord
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = "It was a queer sultry summer the summer they electrocuted the Rosenbergs and I didn’t know what I was doing in New York";
+            Console.WriteLine(RepeatedWord(str));
+        }
+
+        public static string RepeatedWord(string book)
+        {
+            string[] words = book.Split(' ');
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (words[i] == words[j]) return words[i];
+                }
+            }
+            return "No repeated words";
         }
     }
 }
