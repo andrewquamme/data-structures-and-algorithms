8d19e61c1356947475a74f4663b2d8a724542bac
andrewquamme <andrewquamme@gmail.com>
2019-02-18 20:12:41 -0800

    group tests for easier reading

--- Data-Structures/Hashtable/HashtableTDD/UnitTest1.cs
+++ Data-Structures/Hashtable/HashtableTDD/UnitTest1.cs
@@ -6,84 +6,96 @@ namespace HashtableTDD
 {
     public class HashtableTDD
     {
-        [Fact]
-        public void TestHash()
+        public class HashTests
         {
-            Hashtable testHT = new Hashtable(1024);
-            Assert.Equal(377, testHT.Hash("brainy"));
+            [Fact]
+            public void TestHash()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                Assert.Equal(377, testHT.Hash("brainy"));
+            }
         }
 
-        [Fact]
-        public void TestAdd()
+        public class AddTests
         {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test");
-            int index = testHT.Hash("brainy");
-            Assert.Equal("test", testHT.HT[index].Value);
-        }
+            [Fact]
+            public void TestAdd()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test");
+                int index = testHT.Hash("brainy");
+                Assert.Equal("test", testHT.HT[index].Value);
+            }
 
-        [Fact]
-        public void TestAddCollision()
-        {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test1");
-            testHT.Add("binary", "test2");
-            int index = testHT.Hash("brainy");
-            Assert.Equal("test2", testHT.HT[index].Value);
-        }
+            [Fact]
+            public void TestAddCollision()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test1");
+                testHT.Add("binary", "test2");
+                int index = testHT.Hash("brainy");
+                Assert.Equal("test2", testHT.HT[index].Value);
+            }
 
-        [Fact]
-        public void TestAddMultiCollisions()
-        {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test1");
-            testHT.Add("binary", "test2");
-            testHT.Add("nibray", "test3");
-            int index = testHT.Hash("brainy");
-            Assert.Equal("test1", testHT.HT[index].Next.Next.Value);
+            [Fact]
+            public void TestAddMultiCollisions()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test1");
+                testHT.Add("binary", "test2");
+                testHT.Add("nibray", "test3");
+                int index = testHT.Hash("brainy");
+                Assert.Equal("test1", testHT.HT[index].Next.Next.Value);
+            }
         }
-
-        [Fact]
-        public void TestGet()
+        
+        public class GetTests
         {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test");
-            Assert.Equal("test", testHT.Get("brainy"));
+            [Fact]
+            public void TestGet()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test");
+                Assert.Equal("test", testHT.Get("brainy"));
+            }
         }
-
-        [Fact]
-        public void TestContains()
+        
+        public class ContainsTests
         {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test");
-            Assert.True(testHT.Contains("brainy"));
-        }
+            [Fact]
+            public void TestContains()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test");
+                Assert.True(testHT.Contains("brainy"));
+            }
 
-        [Fact]
-        public void TestContainsWithCollision()
-        {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test1");
-            testHT.Add("binary", "test2");
-            Assert.True(testHT.Contains("brainy"));
-        }
+            [Fact]
+            public void TestContainsWithCollision()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test1");
+                testHT.Add("binary", "test2");
+                Assert.True(testHT.Contains("brainy"));
+            }
 
-        [Fact]
-        public void TestContainsWithMultiCollision()
-        {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test1");
-            testHT.Add("binary", "test2");
-            testHT.Add("nibray", "test3");
-            Assert.True(testHT.Contains("brainy"));
-        }
+            [Fact]
+            public void TestContainsWithMultiCollision()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test1");
+                testHT.Add("binary", "test2");
+                testHT.Add("nibray", "test3");
+                Assert.True(testHT.Contains("brainy"));
+            }
 
-        [Fact]
-        public void TestDoesNotContain()
-        {
-            Hashtable testHT = new Hashtable(1024);
-            testHT.Add("brainy", "test");
-            Assert.False(testHT.Contains("ferret"));
+            [Fact]
+            public void TestDoesNotContain()
+            {
+                Hashtable testHT = new Hashtable(1024);
+                testHT.Add("brainy", "test");
+                Assert.False(testHT.Contains("ferret"));
+            }
         }
     }
 }
