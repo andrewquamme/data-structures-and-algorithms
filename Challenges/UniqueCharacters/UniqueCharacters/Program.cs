81532fe33d4dc847f6fddc352e84ce33146976e3
andrewquamme <andrewquamme@gmail.com>
2019-02-21 17:55:37 -0800

    add hashtable reference and code solution

---
+++ Challenges/UniqueCharacters/UniqueCharacters/Program.cs
@@ -0,0 +1,28 @@
+﻿using HashTable.Classes;
+using System;
+
+namespace UniqueCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UniqueCharacters("brown dog"));
+        }
+
+        public static bool UniqueCharacters(string str)
+        {
+            Hashtable HT = new Hashtable(1024);
+            for (int i = 0; i < str.Length; i++)
+            {
+                string character = Convert.ToString(str[i]);
+
+                if (HT.Contains(character)) return false;
+
+                HT.Add(character, character);
+            }
+
+            return true;
+        }
+    }
+}
