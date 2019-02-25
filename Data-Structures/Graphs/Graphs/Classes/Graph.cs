ab51d70f4be55c72beae7480fa99d4bc21e66856
andrewquamme <andrewquamme@gmail.com>
2019-02-25 11:20:01 -0800

    add methods to Graph class

---
+++ Data-Structures/Graphs/Graphs/Classes/Graph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classes
+{
+    class Graph
+    {
+
+        public void AddNode()
+        {
+
+        }
+
+        public void AddEdge()
+        {
+
+        }
+
+        public void GetNodes()
+        {
+
+        }
+
+        public void GetNeighbors()
+        {
+
+        }
+
+        public void Size()
+        {
+
+        }
+    }
+}
